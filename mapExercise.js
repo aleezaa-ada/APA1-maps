defa08b965fb3fdfd09861666dcc21b1ba8809c3
aleezaa-ada <aleeza.ahmed@ada.ac.uk>
2025-01-15 10:33:22 +0000

    completed mapExercises

--- mapExercise.js
+++ mapExercise.js
@@ -1,22 +1,29 @@
 const catalogue = new Map();
 
 function addBook(bookId, title, author) {
+  catalogue.set(bookId, title, author)
+  console.log(catalogue)
   // TODO: Implement this function to add a book to the catalogue.
 }
 
 function findBook(bookId) {
-  // TODO: Implement this function to find a book by its ID.
+  return catalogue.get(bookId)
 }
 
 function removeBook(bookId) {
+  catalogue.delete(bookId);
+  console.log(catalogue)
   // TODO: Implement this function to remove a book from the catalogue.
 }
 
 function listAllBooks() {
+  const bookTitles = Array.from(catalogue.values()); 
+  console.log("book titles", bookTitles);
   // TODO: Implement this function to list all books in the catalogue.
 }
 
 function numberOfBooks() {
+  return catalogue.size
   // TODO: Implement this function to return the number of books in the catalogue.
 }
 
@@ -28,3 +35,4 @@ console.log(findBook("12345")); // Should output the book object
 removeBook("12345");
 listAllBooks(); // Should list the remaining book(s)
 console.log(numberOfBooks()); // Should output the correct count
+
